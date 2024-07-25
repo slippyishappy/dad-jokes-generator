cdb0f2f2fcc6df369e310282fd8f64aecf428ee0
Slippy <116444403+slippyishappy@users.noreply.github.com>
2024-07-25 12:28:00 +0100

    Restored previous version

--- public/script.js
+++ public/script.js
@@ -1,19 +1,15 @@
 const btn = document.querySelector('#joke-btn');
 const jokeDisplay = document.querySelector('#joke');
 
-async function getJoke() {
-    try {
-      const response = await fetch('/joke');
-      if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
-      }
-      const data = await response.json();
-      jokeDisplay.textContent = data.joke;
-    } catch (error) {
-      console.error('There was a problem with the fetch operation:', error);
+btn.addEventListener('click', async () => {
+  try {
+    const response = await fetch('/joke');
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
     }
-};
-
-btn.addEventListener('click', getJoke());
-
-getJoke();
+    const data = await response.json();
+    jokeDisplay.textContent = data.joke;
+  } catch (error) {
+    console.error('There was a problem with the fetch operation:', error);
+  }
+});
